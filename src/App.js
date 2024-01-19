cd0448402ebcf841b486b1b739f58beea99ac57b
Erki Allekõrs <Erkxalle@gmail.com>
2024-01-19 13:56:46 +0200

    ExpenseData Group

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const data={ 
+    date : new Date(2023, 9, 6),
+    title : 'New book',
+    price : 30.99
+  } 
   return (
     <div className="App">
-      <ExpenseItem />
-      <ExpenseItem />
+      <ExpenseItem 
+        expenseData={data}    
+      />
     </div>
   );
 }
