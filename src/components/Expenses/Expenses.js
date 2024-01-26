ef9977ad6b754790831af7c178cd5d9962444293
Erki Allekõrs <Erkxalle@gmail.com>
2024-01-26 15:19:34 +0200

    Filter Callback to Expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,18 @@
 import './Expenses.css'
+import { useState } from 'react';
 import Card from '../UI/Card'
 import ExpenseItem from './ExpenseItem'
-
+import ExpensesFilter from './ExpensesFilter'
 
 const Expenses = (props) => {
-      
+     const filterChangeHandler = (filteredYear) => {
+            console.log('Year data in Expenses.js '+ filteredYear)
+      }
       return (
             <Card className="expenses">
-           <ExpenseItem expenseData={props.expenses[0]} />
-           <ExpenseItem expenseData={props.expenses[1]} />
+            <ExpensesFilter onChangeFilter={filterChangeHandler}/>
+            <ExpenseItem expenseData={props.expenses[0]} />
+            <ExpenseItem expenseData={props.expenses[1]} />
            </Card>
     ) 
 } 
---
+++ src/components/Expenses/ExpensesFilter.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useState } from 'react';
+import './ExpensesFilter.css';
+
+const ExpensesFilter = (props) => {
+   /* const [selectedYear, setSelectedYear] = useState('2023');
+    //console.log(selectedYear)
+    const saveExpenseDataHandler= (event) => {
+        setSelectedYear(selectedYear)
+        event.preventDefault()
+        props.onChangeFilter(selectedYear)
+    }
+    const handleYearChange = (event) => {
+        const selectedYearValue = event.target.value;
+        console.log('Selected Year:', selectedYearValue);
+        setSelectedYear(selectedYearValue);
+    }
+   
+  */
+const [selectedYear, setSelectedYear] = useState('2023');
+
+const yearChangeHandler = (event) => {
+    const selectedYear = event.target.value;
+    setSelectedYear(selectedYear);
+
+    // Call the callback function with the selected year
+    props.onChangeFilter(selectedYear);
+};
+
+return (
+    <div className='expenses-filter'>
+        <div className='expenses-filter__control'>
+            <label>Filter by year</label>
+            <select value={selectedYear} onChange={yearChangeHandler}>
+                <option value='2023'>2023</option>
+                <option value='2024'>2024</option>
+                <option value='2025'>2025</option>
+            </select>
+        </div>
+    </div>
+);
+};
+
+export default ExpensesFilter;
