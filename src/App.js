ecfd33131783d844f02176de2e697d69aa71a630
Erki Allekõrs <Erkxalle@gmail.com>
2024-01-26 15:31:37 +0200

    setExpenses

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpenses from './components/NewExpense/NewExpenses';
 
-const App = () => {
-  const DYMMY_EXPENSES=[
+const DYMMY_EXPENSES=[
   { 
     id: 'id1',
     date : new Date(2023, 9, 6),
@@ -27,14 +27,18 @@ const App = () => {
   } 
   ]  
 
+const App = () => {
+  const [expenses, setExpenses] = useState(DYMMY_EXPENSES)
+
   const addExpenseHandler = (expense) => {
-    console.log('In App.js')
-    console.log(expense)
+    setExpenses((previousExpenses) => {
+      return [expense, ...previousExpenses]
+    })
   }
   return (
     <div className="App">
       <NewExpenses onAddExpense={addExpenseHandler}></NewExpenses>
-      <Expenses expenses={DYMMY_EXPENSES} />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
